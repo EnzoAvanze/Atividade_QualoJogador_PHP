aff979fdf3d53ee2b4555ee768696e2f205da251
Enzo Avanze <enzavanzesenai@gmail.com>
2025-10-30 15:58:33 -0300

    Add files via upload

---
+++ quizAtvAvanze/data/capitals.php
@@ -0,0 +1,351 @@
+<?php
+return [
+    [
+        "Harry Kane", "Bayern Munchen"
+    ],
+    [
+        "Kylian Mbappé", "Real Madrid"
+    ],
+    [
+        "Marcus Rashford", "Barcelona"
+    ],
+    [
+        "Nico O'Reilly", "Manchester City"
+    ],
+    [
+        "Florian Wirtz", "Liverpool"
+    ],
+    [
+        "Georges Mikautadze", "Villarreal"
+    ],
+    [
+        "Thiago Almada", "Atlético de Madrid"
+    ],
+    [
+        "Marko Dmitrovic", "Espanyol"
+    ],
+    [
+        "Antony", "Real Betis"
+    ],
+    [
+        "Alemão", "Rayo Vallecano"
+    ],
+    [
+        "André Silva", "Elche"
+    ],
+    [
+        "Nico Willians", "Athletic Club"
+    ],
+    [
+        "Mauro Arambarri", "Getafe"
+    ],
+    [
+        "Marcão", "Sevilla"
+    ],
+    [
+        "Lucas Boyé", "Alavés"
+    ],
+    [
+        "Iago Aspas", "Celta Vigo"
+    ],
+    [
+        "Ante Budimir", "Osasuna"
+    ],
+    [
+        "Kervin Arriaga", "Levante UD"
+    ],
+    [
+        "Vedat Muriqi", "Mallorca"
+    ],
+    [
+        "Takefusa Kubo", "Real Sociedad"
+    ],
+    [
+        "Arnaut Danjuma", "Valencia"
+    ],
+    [
+        "Eric Bailly", "Real Oviedo"
+    ],
+    [
+        "Jhon Solís", "Girona FC"
+    ],
+    [
+        "Viktor Gyokeres", "Arsenal FC"
+    ],
+    [
+        "Evanilson", "Bournemouth"
+    ],
+    [
+        "Xavi Simons", "Tottenham"
+    ],
+    [
+        "Granit Xhaka", "Sunderland"
+    ],
+    [
+        "Benjamim Sesko", "Manchester United"
+    ],
+    [
+        "Youri Tielemans", "Aston Villa"
+    ],
+    [
+        "Cole Palmer", "Chelsea"
+    ],
+    [
+        "Jean-Philippe Mateta", "Crystal Palace"
+    ],
+    [
+        "Jordan Henderson", "Brentford"
+    ],
+    [
+        "Nick Woltemade", "Newcastle"
+    ],
+    [
+        "Diego Gómez", "Brighton"
+    ],
+    [
+        "Jordan Pickford", "Everton"
+    ],
+    [
+        "Noah Okafor", "Leeds United"
+    ],
+    [
+        "Lesley Ugochukwu", "Burnley"
+    ],
+    [
+        "Kevin", "Fulham"
+    ],
+    [
+        "Murillo", "Nottingham Forest"
+    ],
+    [
+        "James Ward-Prowse", "West Ham United"
+    ],
+    [
+        "Jhon Arias", "Wolverhampton"
+    ],
+    [
+        "Jobe Bellingham", "Borussia Dortmund"
+    ],
+    [
+        "Timo Werner", "RB Leipzig"
+    ],
+    [
+        "Bilal El Khannouss", "Stuttgart"
+    ],
+    [
+        "Claudio Echeverri", "Bayer 04 Leverkusen"
+    ],
+    [
+        "Mario Götze", "Eintracht Frankfurt"
+    ],
+    [
+        "Andej Kramaric", "TSG Hoffenheim"
+    ],
+    [
+        "Jacub Kaminski", "1. FC Köln"
+    ],
+    [
+        "Yukinari Sugawara", "SV Werder Bremen"
+    ],
+    [
+        "Josip Juranovic", "1. FC Union Berlin"
+    ],
+    [
+        "Matthias Ginter", "SC Freiburg"
+    ],
+    [
+        "Jonas Wind", "VfL Wolfsburg"
+    ],
+    [
+        "Yussuf Poulsen", "Hamburgo SV"
+    ],
+    [
+        "Jackson Irvine", "FC St. Pauli"
+    ],
+    [
+        "Elias Saad", "FC Augsburg"
+    ],
+    [
+        "Armindo Sieb", "1. FSV Mainz 05"
+    ],
+    [
+        "Adam Kölle", "1. FC Heidenheim"
+    ],
+    [
+        "Giovanni Reyna", "Borussia M'gladbach"
+    ],
+    [
+        "Flaco Lopez", "Palmeiras"
+    ],
+    [
+        "Jorge Carrascal", "Flamengo"
+    ],
+    [
+        "Kaio Jorge", "Cruzeiro"
+    ],
+    [
+        "Reinaldo", "Mirassol"
+    ],
+    [
+        "Rodrigo Nestor", "Bahia"
+    ],
+    [
+        "Agustin Cannobio", "Fluminense"
+    ],
+    [
+        "Savarino", "Botafogo"
+    ],
+    [
+        "Rayan", "Vasco da Gama"
+    ],
+    [
+        "Luciano", "São Paulo"
+    ],
+    [
+        "Gui Negão", "Corinthians"
+    ],
+    [
+        "Carlos Vinicius", "Grêmio"
+    ],
+    [
+        "Jhon Jhon", "RB Bragantino"
+    ],
+    [
+        "Dudu", "Atletico MG"
+    ],
+    [
+        "Pedro Raul", "Ceará"
+    ],
+    [
+        "Johan Carbonero", "Internacional"
+    ],
+    [
+        "Neymar Jr", "Santos"
+    ],
+    [
+        "Mateusinho", "Vitória"
+    ],
+    [
+        "Breno Lopes", "Fortaleza"
+    ],
+    [
+        "Jandrei", "Juventude"
+    ],
+    [
+        "Lucas Lima", "Sport Recife"
+    ],
+    [
+        "Kevin de Bruyne", "Napoli"
+    ],
+    [
+        "Artem Dovbyk", "Roma"
+    ],
+    [
+        "Marcus Thuram", "Internazionale"
+    ],
+    [
+        "Luka Modric", "Milan"
+    ],
+    [
+        "Álvaro Morata", "Como FC"
+    ],
+    [
+        "Santiago Castro", "Bologna"
+    ],
+    [
+        "Kenan Yildiz", "Juventus"
+    ],
+    [
+        "Jamie Vardy", "Cremonese"
+    ],
+    [
+        "Charles De Ketelaere", "Atalanta"
+    ],
+    [
+        "Nicolò Zaniolo", "Udinese"
+    ],
+    [
+        "Franco Israel", "Torino"
+    ],
+    [
+        "Boulaye Dia", "Lazio"
+    ],
+    [
+        "Nemanja Matic", "Sassuolo"
+    ],
+    [
+        "Yerry Mina", "Cagliari"
+    ],
+    [
+        "Christian Ordoñez", "Parma"
+    ],
+    [
+        "Francesco Camarda", "Lecce"
+    ],
+    [
+        "Gift Orban", "Hellas Verona"
+    ],
+    [
+        "Juan Cuadrado", "Pisa"
+    ],
+    [
+        "David De Gea", "Fiorentina"
+    ],
+    [
+        "Valentin Carboni", "Genoa"
+    ],
+    [
+        "Vitinha", "PSG"
+    ],
+    [
+        "Folarin Balogun", "AS Monaco"
+    ],
+    [
+        "Leonardo Balerdi", "Olympique de Marseille"
+    ],
+    [
+        "Kendry Páez", "RC Strasbourg"
+    ],
+    [
+        "Corentin Tolisso", "Lyon"
+    ],
+    [
+        "Florian Thauvin", "RC Lens"
+    ],
+    [
+        "Ayyoub Bouaddi", "Lille"
+    ],
+    [
+        "Dante", "Nice"
+    ],
+    [
+        "Guillaume Restes", "Toulouse"
+    ],
+    [
+        "Breel Embolo", "Rennes"
+    ],
+    [
+        "Fodé Doucouré", "Le Havre"
+    ],
+    [
+        "Kevin Trapp", "Paris FC"
+    ],
+    [
+        "Sidiki Cherif", "Angers"
+    ],
+    [
+        "Mama Baldé", "Brest"
+    ],
+    [
+        "Anthony Lopes", "Nantes"
+    ],
+    [
+        "Sambou Soumano", "Lorient"
+    ],
+    [
+        "Ibrahim Osman", "Auxerre"
+    ],
+    [
+        "Habib Diallo", "Metz"
+    ],
+];
